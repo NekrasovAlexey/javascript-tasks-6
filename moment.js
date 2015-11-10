afca0e605ead21fe1796f292fbd13fb2e4bb9841
Алексей <creepy404nf@gmail.com>
2015-11-10 17:54:43 +0500

    add solution

--- moment.js
+++ moment.js
@@ -1,6 +1,64 @@
 'use strict';
 
 module.exports = function () {
+    function dateToInt(date) {
+        var day;
+        switch (date.substr(0, 2)) {
+            case 'ПН':
+                day = 0;
+                break;
+            case 'ВТ':
+                day = 1;
+                break;
+            case 'СР':
+                day = 2;
+                break;
+        }
+
+        return day * 24 + timeToInt(date.substr(3));
+    }
+
+    function timeToInt(time) {
+        var hour = parseInt(time.substr(0, 2));
+        if (time.substr(5, 1) == '+') {
+            hour -= parseInt(time.substr(6, 2));
+        }
+        if (time.substr(5, 1) == '-') {
+            hour += parseInt(time.substr(6, 2));
+        }
+        var minutes = parseInt(time.substr(3, 2)) / 60;
+        return hour + minutes;
+    }
+
+    function intToDate(int) {
+        var date = '';
+        switch (parseInt(int / 24)) {
+            case 0:
+                date += 'ПН';
+                break;
+            case 1:
+                date += 'ВТ';
+                break;
+            case 2:
+                date += 'СР';
+                break;
+        }
+        var time = int % 24;
+        var hour = parseInt(time);
+        if (hour < 10) {
+            date += ' 0' + hour;
+        } else {
+            date += ' ' + hour;
+        }
+        var minutes = (time - hour) * 60;
+        if (minutes < 10) {
+            date += ':0' + minutes;
+        } else {
+            date += ':' + minutes;
+        }
+        return date;
+    }
+
     return {
         // Здесь как-то хранится дата ;)
         date: null,
@@ -10,6 +68,17 @@ module.exports = function () {
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            var format = pattern.slice();
+            var date = intToDate(dateToInt(this.date + this.timezone));
+            format = replaceIntoString(format, '%DD', date.substr(0, 2));
+            format = replaceIntoString(format, '%HH', date.substr(3, 2));
+            format = replaceIntoString(format, '%MM', date.substr(6, 2));
+            return format;
+
+            function replaceIntoString(string, from, to) {
+                var index = string.indexOf(from);
+                return string.slice(0, index) + to + string.slice(index + 3);
+            }
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
--- robbery.js
+++ robbery.js
@@ -5,12 +5,109 @@ var moment = require('./moment');
 // Выбирает подходящий ближайший момент начала ограбления
 module.exports.getAppropriateMoment = function (json, minDuration, workingHours) {
     var appropriateMoment = moment();
-
-    // 1. Читаем json
-    // 2. Находим подходящий ближайший момент начала ограбления
-    // 3. И записываем в appropriateMoment
-
+    var shedule = JSON.parse(json);
+    var freeTime = [{from: 0, to: 72}];
+    var names = Object.keys(shedule);
+    names.forEach(function (name) {
+        var personShedule = shedule[name];
+        personShedule.forEach(function (period) {
+            freeTime = mergeTime(freeTime, {from: dateToInt(period.from),
+                                            to: dateToInt(period.to)});
+        });
+    });
+    var workFrom = timeToInt(workingHours.from);
+    var workTo = timeToInt(workingHours.to);
+    for (var i = 0; i < 3; i++) {
+        freeTime = mergeTime(freeTime, {from: 24 * i, to: workFrom + 24 * i});
+        freeTime = mergeTime(freeTime, {from: workTo + 24 * i, to: 24 * (i + 1)});
+    }
+    freeTime = freeTime.filter(function (period) {
+        return period.to - period.from > minDuration / 60;
+    });
+    appropriateMoment.date = intToDate(freeTime[0].from);
+    appropriateMoment.timezone = 0;
     return appropriateMoment;
+
+    function mergeTime(time, period) {
+        var newTime = time.slice();
+        time.forEach(function (freePeriod) {
+            var newPeriods = [];
+            if (freePeriod.from >= period.to || freePeriod.to <= period.from) {
+                newPeriods.push(freePeriod);
+            }
+            if (freePeriod.from < period.from && freePeriod.to > period.from) {
+                //возможно нужно вычитать минуту
+                newPeriods.push({from: freePeriod.from, to: period.from});
+            }
+            if (freePeriod.to > period.to && freePeriod.from < period.to) {
+                newPeriods.push({from: period.to, to: freePeriod.to});
+            }
+            var index = newTime.indexOf(freePeriod);
+            if (index > -1) {
+                newTime.splice(newTime.indexOf(freePeriod));
+            }
+            newTime = newTime.concat(newPeriods);
+        });
+        return newTime;
+    }
+
+    function dateToInt(date) {
+        var day;
+        switch (date.substr(0, 2)) {
+            case 'ПН':
+                day = 0;
+                break;
+            case 'ВТ':
+                day = 1;
+                break;
+            case 'СР':
+                day = 2;
+                break;
+        }
+
+        return day * 24 + timeToInt(date.substr(3));
+    }
+
+    function timeToInt(time) {
+        var hour = parseInt(time.substr(0, 2));
+        if (time.substr(5, 1) == '+') {
+            hour -= parseInt(time.substr(6, 2));
+        }
+        if (time.substr(5, 1) == '-') {
+            hour += parseInt(time.substr(6, 2));
+        }
+        var minutes = parseInt(time.substr(3, 2)) / 60;
+        return hour + minutes;
+    }
+
+    function intToDate(int) {
+        var date = '';
+        switch (parseInt(int / 24)) {
+            case 0:
+                date += 'ПН';
+                break;
+            case 1:
+                date += 'ВТ';
+                break;
+            case 2:
+                date += 'СР';
+                break;
+        }
+        var time = int % 24;
+        var hour = parseInt(time);
+        if (hour < 10) {
+            date += ' 0' + hour;
+        } else {
+            date += ' ' + hour;
+        }
+        var minutes = (time - hour) * 60;
+        if (minutes < 10) {
+            date += ':0' + minutes;
+        } else {
+            date += ':' + minutes;
+        }
+        return date;
+    }
 };
 
 // Возвращает статус ограбления (этот метод уже готов!)
