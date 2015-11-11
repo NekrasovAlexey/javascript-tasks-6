adac2006d92eff01cc18d395378c3554656eb2b2
Алексей <creepy404nf@gmail.com>
2015-11-11 13:37:20 +0500

    Вынес функции перевода времени в отдельный файл

---
+++ utils.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports.dateToInt = function (date) {
+    var days = ['ПН', 'ВТ', 'СР'];
+    return days.indexOf(date.substr(0, 2)) * 24 + this.timeToInt(date.substr(3));
+};
+
+module.exports.timeToInt = function timeToInt(time) {
+    var hour = parseInt(time.substr(0, 2));
+    if (time.substr(5, 1) == '+') {
+        hour -= parseInt(time.substr(6, 2));
+    }
+    if (time.substr(5, 1) == '-') {
+        hour += parseInt(time.substr(6, 2));
+    }
+    var minutes = parseInt(time.substr(3, 2)) / 60;
+    return hour + minutes;
+};
+
+module.exports.intToDate = function (int) {
+    var days = ['ПН', 'ВТ', 'СР'];
+    var date = days[parseInt(int / 24)];
+    var time = int % 24;
+    var hour = parseInt(time);
+    if (hour < 10) {
+        date += ' 0' + hour;
+    } else {
+        date += ' ' + hour;
+    }
+    var minutes = (time - hour) * 60;
+    if (minutes < 10) {
+        date += ':0' + minutes;
+    } else {
+        date += ':' + minutes;
+    }
+    return date;
+};
